1cb36cfdf18aa1468fdaa5154d098aa7d42fbefa
Mental <Alekov99@mail.ru>
2023-08-19 20:26:56 +0500

    Update rotating cubes

--- main.js
+++ main.js
@@ -2,50 +2,51 @@ import * as THREE from "three";
 import WebGL from "three/addons/capabilities/WebGL.js";
 
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
+camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(),
-  new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-  })
-);
-scene.add(cube);
+const light = new THREE.DirectionalLight(0xffffff, 3);
+light.position.set(-1, 2, 4);
+scene.add(light);
 
-camera.position.z = 30;
+const createCube = (geometry, color, x) => {
+  const material = new THREE.MeshPhongMaterial({ color });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+  cube.position.x = x;
+  return cube;
+};
 
-const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-const points = [
-  new THREE.Vector3(-10, 0, 0),
-  new THREE.Vector3(0, 10, 0),
-  new THREE.Vector3(10, 0, 0),
-  new THREE.Vector3(20, 10, 0),
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubes = [
+  createCube(cubeGeometry, 0x44aa88, 0),
+  createCube(cubeGeometry, 0x8844aa, -2),
+  createCube(cubeGeometry, 0xaa8844, 2),
 ];
-const geometry = new THREE.BufferGeometry().setFromPoints(points);
-const line = new THREE.Line(geometry, material);
-scene.add(line);
 
 let timeLastFrameMS = 1000 / 60;
 let deltaTime = 1 / 60;
-
 const animate = (timeMS) => {
   requestAnimationFrame(animate);
 
   if (timeMS) deltaTime = (timeMS - timeLastFrameMS) / 1000;
 
-  cube.rotation.x += 1 * deltaTime;
-  cube.rotation.z += 1 * deltaTime;
+  cubes.forEach((cube, index) => {
+    const speed = 1 + index * 0.1;
+    cube.rotation.x += speed * deltaTime;
+    cube.rotation.y += speed * deltaTime;
+  });
 
   if (timeMS) timeLastFrameMS = timeMS;
   renderer.render(scene, camera);
